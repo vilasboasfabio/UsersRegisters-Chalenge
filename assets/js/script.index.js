a5380e19b68de76bf4be5d848c56d4be09db2f91
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-08-21 08:30:35 -0300

    feat: creat user's class and constructor.

---
+++ assets/js/script.index.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(name, email, birthdate, city, phone, CPF){
+        this.name = name;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.city = city;
+        this.phone = phone;
+        this.CPF = CPF;
+    }
+
+}
