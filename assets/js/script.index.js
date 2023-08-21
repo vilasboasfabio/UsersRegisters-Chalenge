514e728b82dcdf8d261f403d2fa427c58090272f
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-08-21 08:43:42 -0300

    feat: add calculate age method.

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,11 +1,18 @@
 class User {
-    constructor(name, email, birthdate, city, phone, CPF){
+    constructor(name, email, birthDate, city, phone, cpf){
         this.name = name;
         this.email = email;
-        this.birthdate = birthdate;
+        this.birthdate = birthDate;
         this.city = city;
         this.phone = phone;
-        this.CPF = CPF;
+        this.cpf = cpf;
+        this.age = this.calcAge();
     }
-
+    calcAge(birthDate) {
+        const today = new Date();
+        const birth = new Date(birthDate);
+        const age = today - birth;
+        return Math.floor(age / (1000 * 60 * 60 * 24 * 365.25));
+      }
+   
 }
